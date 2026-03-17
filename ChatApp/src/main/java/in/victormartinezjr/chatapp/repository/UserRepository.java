45cd918bef3d162baf17f446abb5ff000d24e828
Victor <victorm.martinezjr@outlook.com>
2026-03-17 02:09:03 -0400

    create User Repo

---
+++ ChatApp/src/main/java/in/victormartinezjr/chatapp/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package in.victormartinezjr.chatapp.repository;
+
+import in.victormartinezjr.chatapp.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
+    Optional<UserEntity> findByUsername(String username);
+}
