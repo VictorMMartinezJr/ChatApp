9d547d43d4479a123d3af833b17a2a94190b3ca4
Victor <victorm.martinezjr@outlook.com>
2026-03-17 23:56:29 -0400

    add validation to ProfileRequest

--- ChatApp/src/main/java/in/victormartinezjr/chatapp/io/ProfileRequest.java
+++ ChatApp/src/main/java/in/victormartinezjr/chatapp/io/ProfileRequest.java
@@ -1,5 +1,9 @@
 package in.victormartinezjr.chatapp.io;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +12,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProfileRequest {
+    @NotBlank(message = "Name cannot be empty")
     private String name;
+
+    @Email(message = "Please enter a valid email")
+    @NotNull(message = "Email cannot be empty")
     private String email;
+
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     private String password;
+    @NotBlank(message = "Username cannot be empty")
     private String username;
 }
